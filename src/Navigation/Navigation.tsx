c76ee0efd662d42ed3216618871b0c9cf1d52189
shvelo012 <shvelidzesaba09@gmail.com>
2024-03-24 01:29:44 +0400

    not so much

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import { loadavg } from 'os';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 export function Navigation() {
+  const [location, setLocation] = useState<any>(null);
+
+  console.log(location.latitude, location.longitude);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      return;
+    }
+
+    const onSuccess = (position: any) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      setLocation({ latitude, longitude });
+    };
+
+    navigator.geolocation.getCurrentPosition(onSuccess);
+  }, []);
+
   return (
     <Router>
       <Routes>

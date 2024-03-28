69413fd79bab5db42e8b34072e9c915cdba5520d
shvelo012 <shvelidzesaba09@gmail.com>
2024-03-28 20:38:12 +0400

    Created basic style for 5 Day forecast

--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,5 +1,14 @@
-export default function TimeLine() {
-  return <div className="bg-[#D9D9D9] w-[25rem] h-[20rem] rounded-3xl shadow-[8px_8px_2px_1px_rgba(41,41,41,0.6)]">
+import DailyRowForecast from "../DailyRowForecast/DailyRowForecast";
 
+export default function TimeLine() {
+  return <div className="bg-[#D9D9D9] w-[25rem] h-[22rem] rounded-3xl shadow-[8px_8px_2px_1px_rgba(41,41,41,0.6)]">
+    <p className="flex justify-center mt-4 font-bold text-3xl " >5 Days Forecast</p>
+    <div>
+      <DailyRowForecast />
+      <DailyRowForecast />
+      <DailyRowForecast />
+      <DailyRowForecast />
+      <DailyRowForecast />
+    </div>
   </div>;
 }
